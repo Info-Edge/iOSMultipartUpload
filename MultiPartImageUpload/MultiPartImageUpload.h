c7f5c95116a9283ec614117fecce1203c202bc85
HarshDuggal <HarshDuggal@users.noreply.github.com>
2014-08-18 11:30:18 +0530

    Update MultiPartImageUpload.h
    Code clean

--- MultiPartImageUpload/MultiPartImageUpload.h
+++ MultiPartImageUpload/MultiPartImageUpload.h
@@ -1,9 +1,9 @@
 //
-//  MultiPartImageUpload.h
-//  MultiPartImageUpload
+//  HDMultiPartImageUpload.h
+//  HDMultiPartImageUpload
 //
 //  Created by HarshDuggal on 13/08/14.
-//  Copyright (c) 2014 HDDev. All rights reserved.
+//  Copyright (c) 2014 Infoedge. All rights reserved.
 //
 //
 //This program is free software: you can redistribute it and/or modify
@@ -19,39 +19,35 @@
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-
-
-
+ 
+ 
+ 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-
-
-#define NOTIFY_AND_LEAVE(X) {[self cleanup:X]; return;}
-#define DATA(X)	[X dataUsingEncoding:NSUTF8StringEncoding]
-
-//static const int oneChunkSize = (1024 * 4);//kAFUploadStream3GSuggestedPacketSize
-
-// Posting constants
-//#define IMAGE_CONTENT @"Content-Disposition: form-data; name=\"%@\"; filename=\"image.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n"
+ 
+ 
+ 
+//static const int oneChunkSize = (1024 * 4);
+ 
 #define BOUNDARY @"--ARCFormBoundaryb6kap934u6jemi"
-
+ 
 #define uploadingImageMappingKeyOnServer @"file" //Use Same key string which your read as input for image filename at your server implementation
-
+ 
 typedef enum : NSUInteger {
     eImageTypePNG,
     eImageTypeJPG,
 } eImageType;
-
-
-
-@interface MultiPartImageUpload : NSObject
-
+ 
+ 
+ 
+@interface HDMultiPartImageUpload : NSObject
+ 
 @property (nonatomic,assign) int                  oneChunkSize;
 @property (nonatomic,assign) eImageType           selectedImageType;
 @property (nonatomic,strong) NSString            *imageFilePath;
 @property (nonatomic,strong) NSString            *uploadURLString;
 @property (nonatomic,strong) NSMutableDictionary *postParametersDict;
-
+ 
 -(void)startUploadImagesToServer;
-
+ 
 @end
